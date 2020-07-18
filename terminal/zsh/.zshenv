1800f4b51e5694f0970af506988cdf0e75202d9b
Jhoan Montoya <jhoanm09@gmail.com>
2020-07-17 19:45:25 -0500

    Primera configuracion dotfiles

---
+++ terminal/zsh/.zshenv
@@ -0,0 +1,5 @@
+#Dotfiles Dir
+export DOTFILES_PATH=$HOME/.dotfiles
+# Define Zim location
+: ${ZIM_HOME=${ZDOTDIR:-${HOME}}/.zim}
+# }}} End configuration added by Zim install
---
+++ terminal/zsh/key-bindings.zsh
@@ -0,0 +1,18 @@
+#Quick access
+_reverse_search() {
+    selected_command=$(fc -rl 1 | awk '{$1="";print substr($0,2)}' | fzf)
+
+    echo -n $selected_command
+}
+
+zle -N _reverse_search
+bindkey '^r' _reverse_search
+
+_change_theme() {
+  selected_command=$(.local/bin/alacritty-colorscheme -C .eendroroy-alacritty-theme/themes -T)
+
+  echo -n $selected_command
+}
+
+zle -N _change_theme
+bindkey '^t' _change_theme
